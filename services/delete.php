11a4311f1fb35ed1d4f4d9685989bf89c6444397
yasminsitaa <rallas674@gmail.com>
2023-10-03 11:39:58 -0300

    Add delete

--- services/delete.php
+++ services/delete.php
@@ -1,13 +1,19 @@
 <?php
-include('../database/db.php')
+include('../database/db.php');
 
 
+$registros=mysqli_query($conexion,"select id_libros from t_libros where id_libros='$_REQUEST[id_libros]'");
 
-
-
-
-
-
+if($reg=mysqli_fetch_array($registros))
+{
+    mysqli_query($conexion,"delete from t_libros where id_libros='$_request[nombre]'");
+    echo "se efectuó el borrado";
+}
+else
+{
+echo "no existe el id";
+}
+mysqli_close($conexion);
 
 
     ?>
